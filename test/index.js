01c0b48d0ef58f21b87650edba48a5ee1434ed27
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-06-15 19:42:08 -0300

    fix: exclude old out dir for each test

--- test/index.js
+++ test/index.js
@@ -1,13 +1,20 @@
 //@ts-check
 
 const path = require("path")
+const fs = require("fs")
 const { generateIcons } = require("../dist/index.js")
 
 const src = path.resolve(__dirname, "./svg")
 const out = path.resolve(__dirname, "./out")
 
+if (fs.existsSync(out)) {
+    fs.rmdirSync(out, { recursive: true })
+}
+
+fs.mkdirSync(out)
+
 generateIcons({
     srcDir: src,
     outDir: out,
-    exporterPath: path.resolve(__dirname, "./out/index.js"),
+    exporterPath: path.resolve(__dirname, "./out/index.js")
 })
